16b0d01419855f248a35140f27579fec2debf93a
Mayowa Makinde <Mayowamakinde53@gmail.com>
2017-03-16 23:04:51 +0100

    js integration

--- js/calculator.js
+++ js/calculator.js
@@ -1,48 +1,95 @@
-function employee(level){
-  this.level=level
+function Employee(level){
+	this.baseSalary = 10000;
+	this.level = level;
 }
-employee.prototype.calcsalary= function(){
-	if (this.level== "level 1"){
-	      return this.baseSalary
-	} else if ( this.level=='level 2'){
-		return (this.baseSalary*1.1);
-	} else if ( this.level =='level 3'){
-		return (this.baseSalary*1.2)
+Employee.prototype.calcsalary= function(){
+	if (this.level== "level1"){
+		console.log(this.baseSalary);
+	    return this.baseSalary
+	} else if ( this.level=='level2'){
+		return (this.baseSalary * 1.1);
+	} else if ( this.level =='level3'){
+		return (this.baseSalary * 1.2)
 	}
+} 
+
+function createEmployee (type, level) {
+	var employee;
+	switch (type) {
+		case "interns":
+			employee = new Interns(level);
+			console.log(employee.level);
+			break;
+		case "associates":
+			employee = new Associates(level);
+			break;
+		case "managers":
+			employee = new Managers(level);
+			break;
+		case "directors":
+			employee = new Director(level);
+			console.log(employee);
+			break;
+		case "executives":
+			employee = new Executive(level);
+			break;
+		default:
+			employee = new employee(level);
+			console.log(employee);
+	}
+	return employee;
 }
-function Interns (level){
-	employee.call(level)
-	this.baseSalary= 10000
+function Interns (level) {
+	var employee = new Employee(level);
+	employee.baseSalary = 10000;
+	return employee;
 }
-Interns.prototype = new employee()
+// Interns.prototype = new employee()
+console.log(Interns.prototype);
 function Associates (level){
-	employee.call(level)
-	this.baseSalary=20000
+	var employee = new Employee(level);
+	employee.baseSalary = 20000;
+	return employee;
 
 }
-Associates.prototype = new employee()
+// Associates.prototype = new employee()
 function Managers (level){
-	employee.call(level)
-	this.baseSalary=30000
+	var employee = new Employee(level);
+	employee.baseSalary = 30000;
+	return employee;
 }
-Managers.prototype = new employee()
+// Managers.prototype = new employee()
 function Executive(level){
-	employee.call(level)
-	this.baseSalary=40000
+	var employee = new Employee(level);
+	employee.baseSalary = 40000;
+	return employee;
 }
-Executive.prototype = new employee()
+// Executive.prototype = new employee()
 function Director(level){
-	employee.call(level)
-  this.baseSalary=50000
-}
-Director.prototype = new employee()
-function out(){
-//var epmtype= document.getElementById("employee").value
-//var level= document.getElementById("level").value
-alert(document.querySelector('input[name=experienceExp]:checked').value);
-alert(document.querySelector('input[name=experienceExp]:checked').value);
+	var employee = new Employee(level);
+	employee.baseSalary = 50000;
+	return employee;
+  }
+// Director.prototype = new employee()
+function out(e){
+	if(e.preventDefault) e.preventDefault();
+	//var epmtype= document.getElementById("employee").value
+	//var level= document.getElementById("level").value
+	// console.log('got here');
+	var level = document.querySelector('input[name=experienceExp]:checked').value;
+	var e = document.getElementById('employeeType');
+	var employeeType = e.options[e.selectedIndex].value;
+	// alert(document.querySelector('input[name=experienceExp]:checked').value);
+	// alert(document.querySelector('input[name=experienceExp]:checked').value);
 
-var bola= new emptype(level)
-var a = bola.calcsalary()
-document.getElementById("change").innerHTML=a;
+	var bola = new createEmployee(employeeType, level)
+	var a = bola.calcsalary()
+	document.getElementById("change").innerHTML = a;
+}
+
+var form = document.getElementById('SalaryForm');
+if (form.attachEvent) {
+    form.attachEvent("submit", out);
+} else {
+    form.addEventListener("submit", out);
 }
